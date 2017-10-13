3e26e31188456d9b5d51b6d48f0a2f442faa6aa7
Sean Killeen <SeanKilleen@gmail.com>
2017-10-13 11:35:40 -0400

    Basic dummy repo

--- src/xluhco.web/ShortLinkItem.cs
+++ src/xluhco.web/ShortLinkItem.cs
@@ -22,4 +22,43 @@ namespace xluhco.web
             this.URL = url;
         }
     }
+
+    public interface IShortLinkRepository
+    {
+        List<ShortLinkItem> GetShortLinks();
+        ShortLinkItem GetByShortCode(string shortCode);
+    }
+
+    public class CachedShortLinkFromCsvRepository : IShortLinkRepository
+    {
+        private readonly List<ShortLinkItem> _shortLinks;
+
+        public CachedShortLinkFromCsvRepository()
+        {
+            _shortLinks = new List<ShortLinkItem>()
+            {
+                new ShortLinkItem("sk", "http://SeanKilleen.com"),
+                new ShortLinkItem("skpres", "http://SeanKilleen.com/Presentations"),
+                new ShortLinkItem("du", "http://TheJavaScriptPromise.com"),
+            };
+
+        }
+
+        private void PopulateShortLinks()
+        {
+            //TODO: Implement piece that reads from CSV
+        }
+
+        public List<ShortLinkItem> GetShortLinks()
+        {
+            return _shortLinks; //TODO: populate if empty
+        }
+
+        public ShortLinkItem GetByShortCode(string shortCode)
+        {
+            return _shortLinks
+                .FirstOrDefault(x => x.ShortLinkCode.Equals(shortCode, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+
 }
