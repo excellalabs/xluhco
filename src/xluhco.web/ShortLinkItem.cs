fd256ac8bbd4033377c39f132710b30d15ef58e0
Sean Killeen <SeanKilleen@gmail.com>
2017-10-13 11:07:45 -0400

    add basic POCO

---
+++ src/xluhco.web/ShortLinkItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xluhco.web
+{
+    public interface IShortLinkItem
+    {
+        string ShortLinkCode { get; }
+        string URL { get; }
+    }
+
+    public class ShortLinkItem : IShortLinkItem
+    {
+        public string ShortLinkCode { get; private set; }
+        public string URL { get; private set; }
+
+        public ShortLinkItem(string shortLinkCode, string url)
+        {
+            this.ShortLinkCode = shortLinkCode;
+            this.URL = url;
+        }
+    }
+}
