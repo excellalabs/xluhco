a31eb20386a7df627f1cd116f440d747511af772
Sean Killeen <SeanKilleen@gmail.com>
2017-10-13 11:54:23 -0400

    dummy redirect controller

---
+++ src/xluhco.web/Controllers/RedirectController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xluhco.web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Redirect")]
+    public class RedirectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello there!");
+        }
+    }
+}
