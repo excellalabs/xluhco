a743ca49dd91ea13a8e040943be6cf65bdf36aa1
Sean Killeen <SeanKilleen@gmail.com>
2017-10-13 12:30:18 -0400

    Add logging

--- src/xluhco.web/Program.cs
+++ src/xluhco.web/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace xluhco.web
 {
@@ -14,6 +16,12 @@ namespace xluhco.web
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
             BuildWebHost(args).Run();
         }
 
