34ace481d003684e51f9dc604fe86d655bd32a9d
Sean Killeen <SeanKilleen@gmail.com>
2017-10-13 12:42:53 -0400

    Attempted bettwe rewrite rules
    Based on
    https://docs.microsoft.com/en-us/iis/extensions/url-rewrite-module/creating-rewrite-rules-for-the-url-rewrite-module

--- src/xluhco.web/Startup.cs
+++ src/xluhco.web/Startup.cs
@@ -41,7 +41,7 @@ namespace xluhco.web
             // regex test URL:
             // http://regexstorm.net/tester?p=%2f%28.*%29&i=seank.com%2fHeyThere&r=%2fapi%2fRedirect%2f%241
             var rewriteOptions = new RewriteOptions()
-                .AddRewrite(@"/(.*)", "/api/Redirect/$1",  skipRemainingRules: true);
+                .AddRewrite(@"^go/(.*)", "api/Redirect/{R:1}",  skipRemainingRules: true);
             app.UseRewriter(rewriteOptions);
             app.UseMvc();
 
