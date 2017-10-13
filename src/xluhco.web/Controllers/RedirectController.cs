973e21565f94369cc3035356520b9a166ee957ba
Sean Killeen <SeanKilleen@gmail.com>
2017-10-13 12:56:25 -0400

    Adding injection of Repository

--- src/xluhco.web/Controllers/RedirectController.cs
+++ src/xluhco.web/Controllers/RedirectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace xluhco.web.Controllers
 {
@@ -11,10 +12,17 @@ namespace xluhco.web.Controllers
     [Route("api/Redirect/{shortCode}")]
     public class RedirectController : Controller
     {
+        private IShortLinkRepository _shortLinkRepo;
+
+        public RedirectController(IShortLinkRepository shortLinkRepo)
+        {
+            _shortLinkRepo = shortLinkRepo;
+        }
         [HttpGet]
         public IActionResult Index(string shortCode)
         {
-            return Ok($"Hello there! Redirecting for short code {shortCode}");
+            var redirectUrl = _shortLinkRepo.GetByShortCode(shortCode)?.URL;
+            return Ok($"Hello there! Redirecting to {redirectUrl}");
         }
     }
 }
--- src/xluhco.web/Startup.cs
+++ src/xluhco.web/Startup.cs
@@ -27,6 +27,7 @@ namespace xluhco.web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IShortLinkRepository, CachedShortLinkFromCsvRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
