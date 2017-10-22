ff32af70ce18b1c43649a51d146445aab944fe18
Sean Killeen <SeanKilleen@gmail.com>
2017-10-22 09:59:13 -0400

    build configuration from appsettings.json

--- src/xluhco.web/Startup.cs
+++ src/xluhco.web/Startup.cs
@@ -9,9 +9,14 @@ namespace xluhco.web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
