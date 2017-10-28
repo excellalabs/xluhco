0e4fd33868ca0b5358c2128ac450df1760631d39
Sean Killeen <SeanKilleen@gmail.com>
2017-10-28 11:11:16 -0400

    Inject and use settings in home controller

--- src/xluhco.web/Controllers/HomeController.cs
+++ src/xluhco.web/Controllers/HomeController.cs
@@ -1,20 +1,23 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace xluhco.web.Controllers
 {
     public class HomeController : Controller
     {
         private IShortLinkRepository _repo;
+        private SiteOptions _siteOptions;
 
-        public HomeController(IShortLinkRepository repo)
+        public HomeController(IShortLinkRepository repo, IOptions<SiteOptions> siteOptions)
         {
             _repo = repo;
+            _siteOptions = siteOptions.Value;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_siteOptions);
         }
 
         public IActionResult List()
