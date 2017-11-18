2e20c4ce2b75c3866f08e91e7b92a2fa2e0bde02
Sean Killeen <SeanKilleen@gmail.com>
2017-11-18 18:43:21 -0500

    More tests + setup refactor

--- src/xluhco.web.tests/Controllers/HomeControllerTests.cs
+++ src/xluhco.web.tests/Controllers/HomeControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -9,6 +10,15 @@ namespace xluhco.web.tests.Controllers
 {
     public class HomeControllerTests
     {
+        private Mock<IShortLinkRepository> _mockRepo;
+        private HomeController _sut;
+
+        public HomeControllerTests()
+        {
+            _mockRepo = new Mock<IShortLinkRepository>();
+            _sut = new HomeController(_mockRepo.Object);
+        }
+
         [Fact]
         public void Ctor_NullRepository_ThrowsError()
         {
@@ -20,13 +30,38 @@ namespace xluhco.web.tests.Controllers
         [Fact]
         public void Index_ReturnsIndexView()
         {
-            var sut = new HomeController(new Mock<IShortLinkRepository>().Object);
-
-            var result = sut.Index();
+            var result = _sut.Index();
 
             var viewResult = Assert.IsType<ViewResult>(result);
 
             viewResult.ViewName.Should().Be("Index");
+        }
+
+        [Fact]
+        public void List_ReturnsListView()
+        {
+            _mockRepo.Setup(x => x.GetShortLinks()).Returns(new List<ShortLinkItem>());
+
+            var result = _sut.List();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            viewResult.ViewName.Should().Be("List");
+        }
+
+        [Fact]
+        public void List_NoItems_ReturnsEmptyList()
+        {
+            _mockRepo.Setup(x => x.GetShortLinks()).Returns(new List<ShortLinkItem>());
+
+            var result = _sut.List();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = Assert.IsAssignableFrom<IEnumerable<ShortLinkItem>>(
+                viewResult.ViewData.Model);
+
+            model.Should().BeEmpty();
         }
     }
 }
