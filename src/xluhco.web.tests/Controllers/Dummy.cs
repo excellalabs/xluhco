8a6eb131af49d92567c756e78c8f4989d14812a4
Sean Killeen <SeanKilleen@gmail.com>
2017-11-19 08:08:51 -0500

    Graduate Dummy.cs

---
+++ src/xluhco.web.tests/Controllers/Dummy.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+
+namespace xluhco.web.tests.Controllers
+{
+    public static class Dummy
+    {
+        public static T Of<T>() where T : class
+        {
+            return new Mock<T>().Object;
+        }
+
+    }
+}
--- src/xluhco.web.tests/Controllers/RedirectControllerTests.cs
+++ src/xluhco.web.tests/Controllers/RedirectControllerTests.cs
@@ -122,14 +122,4 @@ namespace xluhco.web.tests.Controllers
             }
         }
     }
-
-    public static class Dummy
-    {
-        public static T Of<T>() where T : class
-        {
-            return new Mock<T>().Object;
-        }
-
-    }
-
 }
