112d550c1a3e0675033035ac2923eb941c1ac930
Sean Killeen <SeanKilleen@gmail.com>
2017-11-25 22:33:59 -0500

    Test that uses setting

---
+++ src/xluhco.web.tests/Integration/ShortLinkFromCsvRepositoryIntegrationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Serilog;
+using Xunit;
+
+namespace xluhco.web.tests.Integration
+{
+    public class ShortLinkFromCsvRepositoryIntegrationTests
+    {
+        private readonly string _webRootPath;
+        public ShortLinkFromCsvRepositoryIntegrationTests()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            var hostingEnv = (IHostingEnvironment)server.Host.Services.GetService(typeof(IHostingEnvironment));
+            this._webRootPath = hostingEnv.WebRootPath;
+        }
+
+        [Fact]
+        public void SettingHasWwwRoot()
+        {
+            _webRootPath.Should().NotBeEmpty();
+            _webRootPath.Should().Contain("wwwroot");
+        }
+    }
+}
