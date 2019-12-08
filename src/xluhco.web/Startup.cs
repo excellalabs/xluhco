9fed34f0f800c1e9e2aa0daa4334b7763565205d
Sean Killeen <SeanKilleen@gmail.com>
2019-12-08 18:48:04 -0500

    remove unnecessary parameter

--- src/xluhco.web/Startup.cs
+++ src/xluhco.web/Startup.cs
@@ -84,7 +84,7 @@ namespace xluhco.web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
