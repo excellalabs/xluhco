4308da1fab8cc55c5f8ded13ad994d2e68701f77
Sean Killeen <SeanKilleen@gmail.com>
2023-06-07 11:21:31 -0400

    startup syntax

--- src/xluhco.web/Program.cs
+++ src/xluhco.web/Program.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 namespace xluhco.web
 {
     public class Program
     {
-        public async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var startedApp = await Host.CreateDefaultBuilder(args)
                 .UseSerilog().StartAsync();
