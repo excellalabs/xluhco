544594202211718e20859dc1f980a3004363a32e
Sean Killeen <SeanKilleen@gmail.com>
2023-06-07 11:53:53 -0400

    Attempts to update startup

--- src/xluhco.web/Program.cs
+++ src/xluhco.web/Program.cs
@@ -1,17 +1,51 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 
 namespace xluhco.web
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var startedApp = await Host.CreateDefaultBuilder(args)
-                .UseSerilog().StartAsync();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-            await startedApp.WaitForShutdownAsync();
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+                builder.Host.UseSerilog(Log.Logger);
+
+                var startup = new Startup(builder.Environment);
+
+                Log.Logger.Information("Configuring services");
+                startup.ConfigureServices(builder.Services);
+
+                Log.Logger.Information("Building app");
+                var app = builder.Build();
+
+                Log.Logger.Information("Configuring for environment");
+                startup.Configure(app, app.Environment);
+
+                Log.Logger.Information("Running");
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         } 
     }
 }
